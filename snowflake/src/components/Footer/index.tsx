ecce50e081c84424aa6c6d766cf04d453a4b28ff
Ram Kumar <ramkumar897003@gmail.com>
2022-01-04 20:49:51 +0100

    updated the home pages

--- snowflake/src/components/Footer/index.tsx
+++ snowflake/src/components/Footer/index.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link, useLocation } from "react-router-dom";
 import { SITE_NAME } from "../../utils/constants";
 
 export default function Footer() {
   const location = useLocation();
+  const [footer, setFooter] = useState('');
+
+  useEffect(() => {
+    if(["/"].includes(location.pathname)) {
+      setFooter('section-dark');
+    } else {
+      setFooter('');
+    }
+  }, [location]);
 
   if (["/login", "/signup", "/404", "/activation"].includes(location.pathname)) {
     return null;
   }
 
   return (
-    <footer className="footer   footer-white ">
+    <footer className={`footer footer-white ${footer}`}>
       <div className="container">
         <div className="row">
           <nav className="footer-nav">
