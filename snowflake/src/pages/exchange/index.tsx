78a11f879408ebe0b7e1511badd78eaee4fa46ca
David Wisner <dwisner6@gmail.com>
2022-01-28 21:43:35 +0800

    attempted import

--- snowflake/src/pages/exchange/index.tsx
+++ snowflake/src/pages/exchange/index.tsx
@@ -1,5 +1,23 @@
+import "./polyfills";
+import "@reach/dialog/styles.css";
+import "inter-ui";
+
 import React from "react";
 
-export default function Exchange() {
-  return <div>Exchange</div>;
+// import { Providers, App, serviceWorkerRegistration } from "@uniswap/interface";
+
+if (!!window.ethereum) {
+  window.ethereum.autoRefreshOnNetworkChange = false;
 }
+export default function Exchange() {
+  return (
+    // <Providers>
+    //   <App />
+    // </Providers>
+    <div>exchange</div>
+  );
+}
+
+// if (process.env.REACT_APP_SERVICE_WORKER !== "false") {
+//   serviceWorkerRegistration.register();
+// }
