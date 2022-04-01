06cc68d048e4ce3981b9150719727284032e0c21
David Wisner <dwisner6@gmail.com>
2022-04-02 01:04:51 +0800

    add webpack

---
+++ snowflake/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const mode = process.env.NODE_ENV || "development";
+const prod = mode === "production";
+
+module.exports = {
+  entry: "./main.js",
+  watch: true,
+  resolve: {
+    alias: {
+      assert: "assert",
+      buffer: "buffer",
+      crypto: "crypto-browserify",
+      http: "stream-http",
+      https: "https-browserify",
+      os: "os-browserify/browser",
+      process: "process/browser",
+      stream: "stream-browserify",
+      util: "util",
+    },
+    extensions: [".mjs", ".js"],
+    mainFields: ["browser", "module", "main"],
+  },
+  output: {
+    path: path.join(__dirname, "/flake-wup.webflow/js"),
+    filename: "[name].js",
+    chunkFilename: "[name].[id].js",
+    publicPath: "/flake-wup.webflow/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  mode,
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process",
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
+  devtool: prod ? false : "source-map",
+  devServer: {
+    hot: false,
+    liveReload: true,
+    static: {
+      directory: path.join(__dirname, "flake-wup.webflow"),
+    },
+    watchFiles: ["flake-wup.webflow/js/**"],
+  },
+};
